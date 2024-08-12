7498a93358fd72c783d81afeced3d3d17d212d07
Grushenka <grushnk.code@gmail.com>
2024-08-12 12:48:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
         { date: "2024-07-30", speaker: "Shawn Foster", title: "How To Present", description: "TBD" },
         { date: "2024-08-06", speaker: "Gunner Gunn", title: "Character Design Is My Passion", description: "Ever wonder what makes youre favourite cartoon characters so memorable? In this presentation I will go over the techniques artists use every day to design such iconic characters!" },
         { date: "2024-08-13", speaker: "James Johnson", title: "TBD", description: "TBD" },
-        { date: "2024-08-20", speaker: "Allyson Steward", title: "The History of Fan Fiction", description: "TBD" },
+        { date: "2024-08-20", speaker: "N/A", title: "", description: "Cancelled this week" },
         { date: "2024-08-27", speaker: "Shiv Kushwala", title: "TBD", description: "TBD" },
+        { date: "2024-09-", speaker: "Allyson Steward", title: "The History of Fan Fiction", description: "TBD" }
         
         // Additional talks can be added here
     ];
